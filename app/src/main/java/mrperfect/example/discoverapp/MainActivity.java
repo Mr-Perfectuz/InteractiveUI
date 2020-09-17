c339d7a1ef702fff249c60f583c69c34ea056622
Mr.Perfect <ziyodovanvar1999@gmail.com>
2020-09-18 00:21:37 +0500

    Project has been updated

--- app/src/main/java/mrperfect/example/discoverapp/MainActivity.java
+++ app/src/main/java/mrperfect/example/discoverapp/MainActivity.java
@@ -2,13 +2,30 @@ package mrperfect.example.discoverapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    LinearLayout backgroundcheck;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        backgroundcheck = (LinearLayout) findViewById(R.id.bg_check);
+        backgroundcheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+
+                Toast.makeText(MainActivity.this, "You clicked New Arrival Activity", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
---
+++ app/src/main/java/mrperfect/example/discoverapp/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package mrperfect.example.discoverapp.ui.main;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
